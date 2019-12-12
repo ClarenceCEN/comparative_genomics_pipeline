cc564f1281ce42ae5065f02d93baca81a7c47ec4
ClarenceCen <clarence_cen@yeah.net>
2019-12-12 19:18:40 +0800

    Update file_name_join_orth.sh

--- pan_genome/file_name_join_orth.sh
+++ pan_genome/file_name_join_orth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 file_name=''
-for file in ./input/*.function
+for file in ./*.fasta
 do
 name=`basename $file`
-file_name=${file_name}"+"${name%.*}
+file_name=${file_name}","${name}
 done
 echo $file_name
